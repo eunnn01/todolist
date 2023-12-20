b8e0b887828b566e688eee08e4a42922c6a89c71
eunnn <eunjae5180@gmail.com>
2023-12-20 11:18:12 +0900

    v1.0

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="ClearAllContainer">
+        <span class="ClearAllBtn" @click="clearTodo">Clear All</span>
+    </div>
+</template>
+<script>
+export default{
+    methods: {
+        clearTodo(){
+            this.$emit('removeAll');
+        }
+    }
+}
+</script>
+
+<style>
+    .ClearAllContainer{
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: white;
+        border-radius: 5px;
+        margin: 0 auto;
+        margin-top: 20px;
+    }
+    .ClearAllBtn{
+        color: #e20303;
+        display: block;
+    }
+</style>
